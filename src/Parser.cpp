b70152a9e847a2cf934d0a489867a82c15f1e98d
Jeromos Kovács <iitsnotme214@proton.me>
2025-04-16 15:31:14 +0200

    refactor(parser): add `this` where possible

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -12,13 +12,13 @@ void ClapParser::parse(const int& argc, char* argv[]) {
     std::vector<std::string> args(argv + 1, argv + argc);
     std::unordered_set<std::string> args_with_values;
 
-    apply_defaults();
-    check_env();
-    parse_options(args);
-    // parse_positional_args(args);
+    this->apply_defaults();
+    this->check_env();
+    this->parse_options(args);
+    // this->parse_positional_args(args);
 
     // Validate all arguments that need values received them
-    for (const auto& arg : args_) {
+    for (const auto& arg : this->args_) {
         if (arg.get__takes_value() && args_with_values.count(arg.get__name()) == 0) {
             if (arg.get__is_required() && !arg.has_default()) {
                 throw std::runtime_error("argument '" + arg.get__name() + "' requires a value");
@@ -26,19 +26,19 @@ void ClapParser::parse(const int& argc, char* argv[]) {
         }
     }
 
-    check_required_args();
+    this->check_required_args();
 }
 
-void ClapParser::add_arg(const Arg& arg) { args_.push_back(arg); }
+void ClapParser::add_arg(const Arg& arg) { this->args_.push_back(arg); }
 
 void ClapParser::parse_options(const std::vector<std::string>& args) {
     for (size_t i = 0; i < args.size(); ++i) {
         const std::string& token = args[i];
 
-        if (is_long_option(token)) {
-            i = handle_long_option(token, args, i);
-        } else if (is_short_option(token)) {
-            i = handle_short_option(token, args, i);
+        if (this->is_long_option(token)) {
+            i = this->handle_long_option(token, args, i);
+        } else if (this->is_short_option(token)) {
+            i = this->handle_short_option(token, args, i);
         } else {
             // Positional arguments are handled separately
             break;
@@ -47,17 +47,17 @@ void ClapParser::parse_options(const std::vector<std::string>& args) {
 }
 
 void ClapParser::check_env() {
-    for (auto& arg : args_) {
+    for (auto& arg : this->args_) {
         if (arg.auto_env_) {
             std::string env_name = PROGRAM_NAME() + '_' + arg.get__name();
             std::transform(env_name.begin(), env_name.end(), env_name.begin(), [](const unsigned char& c) { return std::toupper(c); });
             auto value_from_env = std::getenv(env_name.c_str());
             if (value_from_env) {
-                values_[arg.get__name()] = value_from_env;
+                this->values_[arg.get__name()] = value_from_env;
             }
         }
         if (arg.has_env() && arg.value_.has_value()) {
-            values_[arg.get__name()] = arg.value_.value();
+            this->values_[arg.get__name()] = arg.value_.value();
         }
     }
 };
@@ -84,8 +84,8 @@ void ClapParser::check_env() {
 // }
 
 void ClapParser::check_required_args() {
-    for (const auto& arg : args_) {
-        if (arg.get__is_required() && values_.find(arg.get__name()) == values_.end()) {
+    for (const auto& arg : this->args_) {
+        if (arg.get__is_required() && this->values_.find(arg.get__name()) == this->values_.end()) {
             throw std::runtime_error("missing required argument: " + arg.get__name());
         }
     }
@@ -94,18 +94,18 @@ void ClapParser::check_required_args() {
 size_t ClapParser::handle_long_option(const std::string& token, const std::vector<std::string>& args, size_t i) {
     std::string opt_name = token.substr(2);
     if (opt_name == "help") {
-        print_help();
+        this->print_help();
         exit(0);
     }
-    const Arg* arg = find_option(opt_name);
+    const Arg* arg = this->find_option(opt_name);
     if (arg == nullptr) {
         throw std::runtime_error("unknown option: " + token);
     }
 
     if (arg->get__takes_value()) {
-        i = handle_option_with_value(arg, args, i, token);
+        i = this->handle_option_with_value(arg, args, i, token);
     } else {
-        values_[arg->get__name()] = true; // Boolean flag
+        this->values_[arg->get__name()] = true; // Boolean flag
     }
 
     return i;
@@ -114,18 +114,18 @@ size_t ClapParser::handle_long_option(const std::string& token, const std::vecto
 size_t ClapParser::handle_short_option(const std::string& token, const std::vector<std::string>& args, size_t i) {
     std::string opt_name = token.substr(1);
     if (opt_name == "h") {
-        print_help();
+        this->print_help();
         exit(0);
     }
-    const Arg* arg = find_option(opt_name);
+    const Arg* arg = this->find_option(opt_name);
     if (arg == nullptr) {
         throw std::runtime_error("unknown option: " + token);
     }
 
     if (arg->get__takes_value()) {
-        i = handle_option_with_value(arg, args, i, token);
+        i = this->handle_option_with_value(arg, args, i, token);
     } else {
-        values_[arg->get__name()] = true; // Boolean flag
+        this->values_[arg->get__name()] = true; // Boolean flag
     }
 
     return i;
@@ -135,12 +135,12 @@ size_t ClapParser::handle_option_with_value(const Arg* arg, const std::vector<st
                                             const std::string& token) {
     if (i + 1 < args.size() && !is_option(args[i + 1])) {
         // Use next argument as value
-        values_[arg->get__name()] = std::string(args[i + 1]);
+        this->values_[arg->get__name()] = std::string(args[i + 1]);
         return i + 1; // Skip the value in the next iteration
     }
     if (arg->has_default()) {
         // Use default value
-        values_[arg->get__name()] = std::string(arg->get__default_value());
+        this->values_[arg->get__name()] = std::string(arg->get__default_value());
     } else {
         throw std::runtime_error("option '" + token + "' requires a value but none was provided");
     }
@@ -153,7 +153,7 @@ void ClapParser::handle_missing_positional(const Arg& arg) {
         throw std::runtime_error("missing required positional argument: " + arg.get__name());
     }
     if (arg.has_default()) {
-        values_[arg.get__name()] = std::string(arg.get__default_value());
+        this->values_[arg.get__name()] = std::string(arg.get__default_value());
     }
 }
 
@@ -169,13 +169,13 @@ void ClapParser::handle_missing_positional(const Arg& arg) {
 
 void ClapParser::print_help() const {
     std::cout << "Usage: " << PROGRAM_NAME() << " [OPTIONS]";
-    auto positionals = get_positional_args();
+    auto positionals = this->get_positional_args();
     for (const auto& pos : positionals) {
         std::cout << " [" << pos.get__name() << "]";
     }
     std::cout << "\n\nOptions:\n";
 
-    for (const auto& arg : args_) {
+    for (const auto& arg : this->args_) {
         arg.get__short_name().empty()? std::cout << "      " : std::cout << "  -" << arg.get__short_name() << ", "; 
         std::cout << "--" << arg.get__long_name();
         std::cout << "\t" << arg.get__help();
@@ -209,7 +209,7 @@ void ClapParser::print_help() const {
 
 // Helper methods
 const Arg* ClapParser::find_option(const std::string& name) const {
-    for (const auto& arg : args_) {
+    for (const auto& arg : this->args_) {
         if (arg.get__long_name() == name || arg.get__short_name() == name) {
             return &arg;
         }
@@ -219,7 +219,7 @@ const Arg* ClapParser::find_option(const std::string& name) const {
 
 std::vector<Arg> ClapParser::get_positional_args() const {
     std::vector<Arg> positional;
-    for (const auto& arg : args_) {
+    for (const auto& arg : this->args_) {
         if (arg.get__short_name().empty() && arg.get__long_name().empty()) {
             positional.push_back(arg);
         }
@@ -228,9 +228,9 @@ std::vector<Arg> ClapParser::get_positional_args() const {
 }
 
 void ClapParser::apply_defaults() {
-    for (const auto& arg : args_) {
-        if (values_.find(arg.get__name()) == values_.end() && arg.has_default()) {
-            values_[arg.get__name()] = std::string(arg.get__default_value());
+    for (const auto& arg : this->args_) {
+        if (this->values_.find(arg.get__name()) == this->values_.end() && arg.has_default()) {
+            this->values_[arg.get__name()] = std::string(arg.get__default_value());
         }
     }
 }
