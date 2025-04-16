3006b4c2e11e56c14ac6b1a7c3c59fcbe1e87fa1
csboo <csongiboo@gmail.com>
2025-04-16 11:28:23 +0200

    feat(example): WIP added example

---
+++ src/example.cpp
@@ -0,0 +1,39 @@
+#include "Parser.hpp"
+#include "Arg.hpp"
+#include "utils.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+void run(const ClapParser& parsed_args);
+
+int main(const int argc, char* argv[]) {
+  ClapParser arg_parser;  
+  auto num1 = Arg("num1").from_env("ASDF").auto_env();
+  // std::cerr << num1 << "\n";
+  arg_parser.add_arg(num1);
+  auto num2 = Arg("num2").short_name("N").from_env("TES").default_value("99");
+  arg_parser.add_arg(num2);
+
+  Arg test("testarg");
+  arg_parser.add_arg(test);
+
+  try {
+    arg_parser.parse(argc, argv);
+    // std::cerr << arg_parser;
+    run(arg_parser);
+  }
+  catch (const std::exception& e) {
+    std::cout << "\n\n\nerror: " << e.what() << "\n\n\n";
+    // arg_parser.print_help();
+  }
+}
+
+void run(const ClapParser& parsed_args) {
+  auto num1 = ok_or_throw_str(parsed_args.get_one_as<int>("num1"), "num1 not defined");
+  auto num2 = ok_or_throw_str(parsed_args.get_one_as<double>("num2"), "num2 not defined");
+
+  std::cout << "num1: " << num1 << '\n';
+  std::cout.precision(5);
+  std::cout << std::fixed << "num2: " << num2 << '\n';
+}
