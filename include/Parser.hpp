52f1f060557985a16c427a51a5de22a441b2a3cd
csboo <csongiboo@gmail.com>
2025-05-14 03:01:50 +0200

    feat(Parser): impl Parse for: int, std::string, bool

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -3,10 +3,41 @@
 #include "Arg.hpp"
 #include "utils.hpp"
 
+#include <cmath>
 #include <iostream>
 #include <optional>
 #include <string>
 #include <vector>
+
+template<typename T>
+struct Parse {
+    static_assert(sizeof(T) == 0, "No Parse<T> specialization defined for this type");
+};
+
+template<>
+struct Parse<int> {
+    static std::optional<int> parse(std::string_view s) {
+        int value;
+        auto [ptr, ec] = std::from_chars(s.data(), s.data() + s.size(), value);
+        if (ec == std::errc()) return value;
+        return std::nullopt;
+    }
+};
+
+template<>
+struct Parse<std::string> {
+    static std::optional<std::string> parse(std::string_view s) {
+        return std::string(s);
+    }
+};
+
+template<>
+struct Parse<bool> {
+    static std::optional<bool> parse(std::string_view s) {
+      auto as_int = Parse<int>::parse(s).value();
+      return as_int;
+    }
+};
 
 class ClapParser {
   public:
