46c3f6e5830180b92a4aa0d54e4ebc959d06fa13
csboo <csongiboo@gmail.com>
2025-06-04 10:36:58 +0200

    fix(Macros): removed that constructor generator that's not used any more

--- include/Macros.hpp
+++ include/Macros.hpp
@@ -44,25 +44,4 @@
         return next;                                                            \
     }
 
-#define ARG_STATE_CONVERSION_CTOR(ALLOW_DIFF_IDX)                                                              \
-    template <typename OSN, typename OLN, typename OH, typename OIR, typename OIF, typename OAM, typename ODV, \
-              typename OFE, typename OAE,                                                                      \
-              std::enable_if_t<(ALLOW_DIFF_IDX != 0 || std::is_same_v<ShortName, OSN>) &&                      \
-                               (ALLOW_DIFF_IDX != 1 || std::is_same_v<LongName, OLN>) &&                       \
-                               (ALLOW_DIFF_IDX != 2 || std::is_same_v<Help, OH>) &&                            \
-                               (ALLOW_DIFF_IDX != 3 || std::is_same_v<IsRequired, OIR>) &&                     \
-                               (ALLOW_DIFF_IDX != 4 || std::is_same_v<IsFlag, OIF>) &&                         \
-                               (ALLOW_DIFF_IDX != 5 || std::is_same_v<AcceptsMany, OAM>) &&                    \
-                               (ALLOW_DIFF_IDX != 6 || std::is_same_v<DefaultValue, ODV>) &&                   \
-                               (ALLOW_DIFF_IDX != 7 || std::is_same_v<FromEnv, OFE>) &&                        \
-                               (ALLOW_DIFF_IDX != 8 || std::is_same_v<AutoEnv, OAE>)>* = nullptr>              \
-    Arg(const Arg<OSN, OLN, OH, OIR, OIF, OAM, ODV, OFE, OAE>& other) {                                        \
-        this->name_ = other.name_;                                                                             \
-        this->long_name_ = other.long_name_;                                                                   \
-        this->help_ = other.help_;                                                                             \
-        this->is_required_ = other.is_required_;                                                               \
-        this->is_flag_ = other.is_flag_;                                                                       \
-        this->accepts_many_ = other.accepts_many_;                                                             \
-        this->value_ = other.value_;                                                                           \
-        this->auto_env_ = other.auto_env_;                                                                     \
     }
